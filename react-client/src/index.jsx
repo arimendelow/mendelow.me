70c8b3b162199b6fa925469b0f0ecd53bc0957be
Ari Mendelow <ari@mendelow.me>
2019-05-31 11:39:38 -0500

    Setup tailwindcss

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './css/tailwind.css';
 import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
