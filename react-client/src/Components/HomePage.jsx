89464fa802e620d0889d46ce37fa4eabb4f44e76
Ari Mendelow <ari@mendelow.me>
2019-05-30 20:26:35 -0500

    Add homepage component

---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+	render() { 
+		return (
+			<React.Fragment>
+				This is my homepage!
+			</React.Fragment>
+		);
+	}
+}
+ 
+export default HomePage;
