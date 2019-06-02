780c8a96c46b1bef662f6b9a9eba27629c25c6f2
Ari Mendelow <ari@mendelow.me>
2019-06-01 22:15:30 -0400

    Add active state in NavBar

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class NavBar extends Component {
-	render() { 
-		const buttonStyleFirst = "text-gray-700 hover:text-gray-500 no-underline";
-		const buttonStyle = "text-gray-700 hover:text-gray-500 no-underline ml-4 md:ml-8";
+	render() {
 		return (
 			<div className="mx-auto px-6 sm:px-8 lg:px-12 xl:px-24 lg:max-w-2lg xl:max-w-6xl">
 				<header className="py-8 text-center sm:pt-16 sm:pb-12 sm:flex sm:items-end sm:justify-between">
-					<Link
+					<NavLink
 						className="inline-flex items-baseline text-white no-underline text-2xl antialiased hover:translateY-1px hover:transition-1s"
 						to="/"
 					>
-						<div className="text-blue-500 mr-0.5">Ari</div>
+						<div className="text-blue-500 mr-1">Ari</div>
 						<div className="font-bold text-yellow-500">Mendelow</div>
-					</Link>
+					</NavLink>
 					<nav id="headerButtons" className="font-medium antialiased mt-4 sm:mt-0">
-						<Link className={buttonStyleFirst} to="/about">About</Link>
-						<Link className={buttonStyle} to="/contact">Contact</Link>
+						<NavLink className="nav-btn" to="/about" activeClassName="active">About</NavLink>
+						<NavLink className="nav-btn" to="/contact" activeClassName="active">Contact</NavLink>
 					</nav>
 				</header>
 			</div>
