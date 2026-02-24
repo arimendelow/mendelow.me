47c0ce4bd023e93a3965b875c4b9787cca9ca1da
Ari Mendelow <arimendelow@clawdbot.noodlefish-goanna.ts.net>
2026-02-24 09:34:39 -0800

    rebrand: Mendelow Studio, remove personal social links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ari Mendelow';
+export const SITE_TITLE = 'Mendelow Studio';
 export const SITE_DESCRIPTION = 'Notes on building, writing, and shipping.';
